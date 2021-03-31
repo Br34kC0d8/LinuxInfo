2e0e57db99acc6d30c548805f637e79395aae856
Br34kC0d8 <81061779+Br34kC0d8@users.noreply.github.com>
2021-03-31 16:14:58 +0000

    Update wrap.py

--- util/wrap.py
+++ util/wrap.py
@@ -9,3 +9,11 @@ class wrap:
         os.system(cmd)
         print('\n')
         sleep(2) 
+        
+    def msgWrap(self,msg,comm):
+        print(msg)
+        sleep(0.5)
+        os.system(comm)
+        sleep(1)
+        os.system('clear')
+
